ed2cfee06ab362bea3d547766d70d24259f8cf48
tian_1800 <tian.sudibyo@gmail.com>
2025-07-17 12:18:40 +0700

    Add redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  );
 }
